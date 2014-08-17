75a74a817d6d2734783956a96a2da8fcf17dd0f3
JAIME.SISTEMA@gmail.com <JAIME.SISTEMA@gmail.com@8e63a9cc-416f-5a56-41ef-5763ba76ccf9>
2014-08-17 23:28:20 +0000

    Criado Projeto - Projeto Final Pizzaria

---
+++ trunk/Projeto Final/src/br/com/pizzaria/conecta.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.pizzaria;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author katia
+ */
+public class conecta {
+    public static Connection getConexão(){
+        try{
+            
+            Connection con;
+            
+            String serverName = "localhost";
+            String mydatabase = "projeto";
+            String userName = "root";
+            String password = "";
+            String driverName = "com.mysql.jdbc.Driver";
+            Class.forName(driverName);
+            String url = "jdbc:mysql://"+serverName+"/"+mydatabase;
+            con = DriverManager.getConnection(url, userName, password);
+            return con;       
+        }catch(ClassNotFoundException e){
+           System.out.println("Driver não encontrado");
+        }catch(SQLException e){
+            System.out.println("Erro ao conectar ao banco");
+        }
+        return null;
+    }
+    
+}
