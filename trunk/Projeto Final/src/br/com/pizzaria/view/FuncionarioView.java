f6cce47bbe9c5020a500cf34afc24b9fb6dac2ff
JAIME.SISTEMA@gmail.com <JAIME.SISTEMA@gmail.com@8e63a9cc-416f-5a56-41ef-5763ba76ccf9>
2014-08-24 03:11:53 +0000

    Alteração das classes FuncionarioView e VerificadoresECorretores

--- trunk/Projeto Final/src/br/com/pizzaria/view/FuncionarioView.java
+++ trunk/Projeto Final/src/br/com/pizzaria/view/FuncionarioView.java
@@ -1,31 +1,24 @@
 package br.com.pizzaria.view;
 
-import br.com.pizzaria.beans.ClienteBeans;
-import br.com.pizzaria.controller.ClienteController;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import javax.swing.JFormattedTextField;
-import javax.swing.JOptionPane;
+import br.com.pizzaria.beans.FuncionarioBeans;
+import br.com.pizzaria.controller.FuncionarioController;
+import br.com.pizzaria.util.VerificadoresECorretores;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.text.MaskFormatter;
 
 public class FuncionarioView extends javax.swing.JInternalFrame {
 
     MaskFormatter FormatoTelefone;
-    SimpleDateFormat FormatoData;
-    Date DataAtual;
-    ClienteBeans clienteBeans;
-    ClienteController clienteController;
+    FuncionarioBeans funcionarioBeans;
+    FuncionarioController funcionarioController;
     DefaultTableModel modelo;
 
     public FuncionarioView() {
         initComponents();
         habilitarCampos(false);
-        FormatoData = new SimpleDateFormat("dd/MM/yyyy");
-
-        clienteBeans = new ClienteBeans();
-        clienteController = new ClienteController();
-        modelo = (DefaultTableModel) tblCliente.getModel();
+        funcionarioBeans = new FuncionarioBeans();
+        funcionarioController = new FuncionarioController();
+        modelo = (DefaultTableModel) tblFucnionario.getModel();
     }
 
     @SuppressWarnings("unchecked")
@@ -38,36 +31,27 @@ public class FuncionarioView extends javax.swing.JInternalFrame {
         jLabel1 = new javax.swing.JLabel();
         txtNome = new javax.swing.JTextField();
         sep_codigo = new javax.swing.JSeparator();
-        lbl_rua = new javax.swing.JLabel();
-        txtRua = new javax.swing.JTextField();
-        txtBairro = new javax.swing.JTextField();
+        lbl_cargo = new javax.swing.JLabel();
         lbl_bairro = new javax.swing.JLabel();
-        lbl_telefone = new javax.swing.JLabel();
         txfPesquisar = new javax.swing.JFormattedTextField();
         lbl_data = new javax.swing.JLabel();
         txtData = new javax.swing.JTextField();
         sep_formulario = new javax.swing.JSeparator();
         lbl_data1 = new javax.swing.JLabel();
-        try{
-
-            FormatoTelefone = new MaskFormatter("(##)####-####");
-
-        }catch (Exception Erro){
-            JOptionPane.showMessageDialog(null, "Telefone inválido", "ERRO DE FORMATAÇÃO", 0);
-        }
-        txfTelefone = new JFormattedTextField(FormatoTelefone);
         sep_pesquisa = new javax.swing.JSeparator();
         jScrollPane1 = new javax.swing.JScrollPane();
-        tblCliente = new javax.swing.JTable();
+        tblFucnionario = new javax.swing.JTable();
         sep_tabela = new javax.swing.JSeparator();
         btnCadastrar = new javax.swing.JButton();
         btnEditar = new javax.swing.JButton();
         btnLiberar = new javax.swing.JButton();
         btnFechar = new javax.swing.JButton();
+        cbCargo = new javax.swing.JComboBox();
+        cbPermissao = new javax.swing.JComboBox();
 
         setClosable(true);
         setIconifiable(true);
-        setTitle("MANUTENÇÃO DE CLIENTE");
+        setTitle("MANUTENÇÃO DE FUNCIONARIO");
         setPreferredSize(new java.awt.Dimension(450, 600));
         try {
             setSelected(true);
@@ -95,14 +79,11 @@ public class FuncionarioView extends javax.swing.JInternalFrame {
         jLabel1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jLabel1.setText("Nome:");
 
-        lbl_rua.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        lbl_rua.setText("Rua:");
+        lbl_cargo.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        lbl_cargo.setText("Cargo:");
 
         lbl_bairro.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        lbl_bairro.setText("Bairro:");
-
-        lbl_telefone.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        lbl_telefone.setText("Telefone:");
+        lbl_bairro.setText("Permissão:");
 
         txfPesquisar.setForeground(new java.awt.Color(0, 51, 255));
         txfPesquisar.setHorizontalAlignment(javax.swing.JTextField.CENTER);
@@ -121,28 +102,29 @@ public class FuncionarioView extends javax.swing.JInternalFrame {
         lbl_data1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         lbl_data1.setText("Pesquisar:");
 
-        tblCliente.setModel(new javax.swing.table.DefaultTableModel(
+        tblFucnionario.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
             new String [] {
-                "Código", "Nome", "Rua", "Bairro", "Telefone"
+                "Código", "Nome", "Cargo", "Permissão"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, true, false, true, false
+                false, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
             }
         });
-        tblCliente.addMouseListener(new java.awt.event.MouseAdapter() {
+        tblFucnionario.getTableHeader().setReorderingAllowed(false);
+        tblFucnionario.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mousePressed(java.awt.event.MouseEvent evt) {
-                tblClienteMousePressed(evt);
+                tblFucnionarioMousePressed(evt);
             }
         });
-        jScrollPane1.setViewportView(tblCliente);
+        jScrollPane1.setViewportView(tblFucnionario);
 
         btnCadastrar.setText("Cadastrar");
         btnCadastrar.setToolTipText("Cadastro de um novo cliente");
@@ -182,34 +164,28 @@ public class FuncionarioView extends javax.swing.JInternalFrame {
             .addComponent(lbl_codigo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(txtCodigo)
             .addComponent(sep_codigo)
+            .addComponent(sep_tabela)
             .addComponent(sep_formulario)
             .addComponent(sep_pesquisa)
-            .addComponent(sep_tabela)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 414, Short.MAX_VALUE)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lbl_rua, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(lbl_bairro, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(lbl_telefone, javax.swing.GroupLayout.Alignment.TRAILING))
+                            .addComponent(lbl_bairro)
+                            .addComponent(lbl_cargo)
+                            .addComponent(jLabel1))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtRua, javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(txtNome, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(txtBairro)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(txfTelefone)
-                                .addGap(18, 18, 18)
+                            .addComponent(cbCargo, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(cbPermissao, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(lbl_data)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(txtData, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(lbl_data1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txfPesquisar))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(btnNovo)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -220,7 +196,11 @@ public class FuncionarioView extends javax.swing.JInternalFrame {
                         .addComponent(btnEditar)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(btnFechar)
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lbl_data1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txfPesquisar)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -237,19 +217,15 @@ public class FuncionarioView extends javax.swing.JInternalFrame {
                     .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbl_rua)
-                    .addComponent(txtRua, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(lbl_cargo)
+                    .addComponent(cbCargo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtBairro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lbl_bairro))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbl_telefone)
+                    .addComponent(lbl_bairro)
+                    .addComponent(cbPermissao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(lbl_data)
-                    .addComponent(txtData, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txfTelefone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addComponent(txtData, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(sep_formulario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -259,7 +235,7 @@ public class FuncionarioView extends javax.swing.JInternalFrame {
                 .addGap(18, 18, 18)
                 .addComponent(sep_pesquisa, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(sep_tabela, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
@@ -276,45 +252,44 @@ public class FuncionarioView extends javax.swing.JInternalFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnNovoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNovoActionPerformed
-        DataAtual = new Date();
-        txtData.setText(FormatoData.format(DataAtual));
+        txtData.setText(VerificadoresECorretores.retornoDeDataAtual());
         habilitarCampos(true);
         txtNome.requestFocus();
-        txtCodigo.setText(clienteController.controleDeCodigo());
+        txtCodigo.setText(funcionarioController.controleDeCodigo());
         limpaNovo();
     }//GEN-LAST:event_btnNovoActionPerformed
 
     private void btnCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarActionPerformed
         populaClienteBeans();
-        if (clienteController.verificarDados(clienteBeans)) {
-            limpaTudo();
-            habilitarCampos(false);
-        }
+//        if (funcionarioController.verificarDados(funcionarioBeans)) {
+//            limpaTudo();
+//            habilitarCampos(false);
+//        }
 
 
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
     private void txfPesquisarKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txfPesquisarKeyReleased
         modelo.setNumRows(0);
-        clienteController.controlePesquisa(txfPesquisar.getText(), modelo);
+        funcionarioController.controlePesquisa(txfPesquisar.getText(), modelo);
     }//GEN-LAST:event_txfPesquisarKeyReleased
 
-    private void tblClienteMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblClienteMousePressed
-        clienteBeans = clienteController.controlePreenchimento(Integer.parseInt(modelo.getValueAt(tblCliente.getSelectedRow(), 0).toString()));
-        txtCodigo.setText(clienteBeans.getCodigo() + "");
-        txtNome.setText(clienteBeans.getNome());
-        txtRua.setText(clienteBeans.getRua());
-        txtBairro.setText(clienteBeans.getBairro());
-        txfTelefone.setText(clienteBeans.getTelefone());
-        txtData.setText(clienteBeans.getDataCad());
-    }//GEN-LAST:event_tblClienteMousePressed
+    private void tblFucnionarioMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblFucnionarioMousePressed
+ //       funcionarioBeans = funcionarioController.controlePreenchimento(Integer.parseInt(modelo.getValueAt(tblFucnionario.getSelectedRow(), 0).toString()));
+        txtCodigo.setText(funcionarioBeans.getCodigo() + "");
+        txtNome.setText(funcionarioBeans.getNome());
+//        txtRua.setText(funcionarioBeans.getRua());
+//        txtBairro.setText(funcionarioBeans.getBairro());
+//        txfTelefone.setText(funcionarioBeans.getTelefone());
+        txtData.setText(funcionarioBeans.getDataCad());
+    }//GEN-LAST:event_tblFucnionarioMousePressed
 
     private void btnEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditarActionPerformed
         populaClienteBeans();
-        if (clienteController.verificarDadosParaEditar(clienteBeans)) {
-            limpaTudo();
-            habilitarCampos(false);
-        }
+//        if (funcionarioController.verificarDadosParaEditar(funcionarioBeans)) {
+//            limpaTudo();
+//            habilitarCampos(false);
+//        }
     }//GEN-LAST:event_btnEditarActionPerformed
 
     private void btnLiberarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLiberarActionPerformed
@@ -332,58 +307,56 @@ public class FuncionarioView extends javax.swing.JInternalFrame {
     private javax.swing.JButton btnFechar;
     private javax.swing.JButton btnLiberar;
     private javax.swing.JButton btnNovo;
+    private javax.swing.JComboBox cbCargo;
+    private javax.swing.JComboBox cbPermissao;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lbl_bairro;
+    private javax.swing.JLabel lbl_cargo;
     private javax.swing.JLabel lbl_codigo;
     private javax.swing.JLabel lbl_data;
     private javax.swing.JLabel lbl_data1;
-    private javax.swing.JLabel lbl_rua;
-    private javax.swing.JLabel lbl_telefone;
     private javax.swing.JSeparator sep_codigo;
     private javax.swing.JSeparator sep_formulario;
     private javax.swing.JSeparator sep_pesquisa;
     private javax.swing.JSeparator sep_tabela;
-    private javax.swing.JTable tblCliente;
+    private javax.swing.JTable tblFucnionario;
     private javax.swing.JFormattedTextField txfPesquisar;
-    private javax.swing.JFormattedTextField txfTelefone;
-    private javax.swing.JTextField txtBairro;
     private javax.swing.JTextField txtCodigo;
     private javax.swing.JTextField txtData;
     private javax.swing.JTextField txtNome;
-    private javax.swing.JTextField txtRua;
     // End of variables declaration//GEN-END:variables
 
     final void habilitarCampos(boolean valor) {
         txtNome.setEditable(valor);
-        txtRua.setEditable(valor);
-        txtBairro.setEditable(valor);
-        txfTelefone.setEditable(valor);
+//        txtRua.setEditable(valor);
+//        txtBairro.setEditable(valor);
+//        txfTelefone.setEditable(valor);
     }
 
     final void populaClienteBeans() {
-        clienteBeans.setNome(txtNome.getText());
-        clienteBeans.setRua(txtRua.getText());
-        clienteBeans.setBairro(txtBairro.getText());
-        clienteBeans.setTelefone(txfTelefone.getText());
-        clienteBeans.setDataCad(txtData.getText());
+        funcionarioBeans.setNome(txtNome.getText());
+//        funcionarioBeans.setRua(txtRua.getText());
+//        funcionarioBeans.setBairro(txtBairro.getText());
+//        funcionarioBeans.setTelefone(txfTelefone.getText());
+        funcionarioBeans.setDataCad(txtData.getText());
 
     }
 
     final void limpaTudo() {
         txtNome.setText("");
-        txtRua.setText("");
-        txtBairro.setText("");
-        txfTelefone.setText("");
+//        txtRua.setText("");
+//        txtBairro.setText("");
+//        txfTelefone.setText("");
         txtCodigo.setText("");
         txtData.setText("");
     }   
     
     final void limpaNovo() {
         txtNome.setText("");
-        txtRua.setText("");
-        txtBairro.setText("");
-        txfTelefone.setText("");
+//        txtRua.setText("");
+//        txtBairro.setText("");
+//        txfTelefone.setText("");
     }   
 
 }
