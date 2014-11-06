f3e82f0390b2e90584592bb7504c294fa1f7d7c2
jaime.sistema@gmail.com <jaime.sistema@gmail.com@8e63a9cc-416f-5a56-41ef-5763ba76ccf9>
2014-11-06 20:06:28 +0000

    Alteração tela de Pedido. Limitado campo login usuario e senha;

--- Projeto Final/src/br/com/pizzaria/util/LimiteDigitos.java
+++ Projeto Final/src/br/com/pizzaria/util/LimiteDigitos.java
@@ -32,7 +32,7 @@ public class LimiteDigitos extends PlainDocument {
         }
         int totalquantia = (getLength() + str.length());
         if (totalquantia <= quantidadeMax) {
-            super.insertString(offset, str.replaceAll("[^a-z|^0-9]",""), attr);
+            super.insertString(offset, str, attr);
             return;
         }
         String nova = str.substring(0, getLength()-quantidadeMax);
